0c4d8fa3ca42be28ff61c250523bd2cfa1da121a
Yonatan Salmon <yonatansalmon@itc.tech>
2023-06-01 17:41:52 +0300

    asfasf

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import { useAppSelector, useAppDispatch } from '../redux/hooks';
 import { supabase } from '../db/supabase';
+import { useNavigate } from 'react-router-dom';
+
 
 const Budget = () => {
   const budget = useAppSelector((state) => state.budget);
+  const navigate = useNavigate();
 
 
   return (
-    <div className='BalanceContainer'>
+    <div className='BalanceContainer' onClick={() => navigate('/dashboard')}>
       <h1>Balance</h1>
       <div className='Balance'>{budget.total}₪</div>
     </div>
